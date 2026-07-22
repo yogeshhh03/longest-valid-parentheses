ab1acb8db7f10f219fe51714f6ccee5af338d87a
yogeshhh03 <yogeshverma1470@gmail.com>
2026-07-23 01:42:31 +0530

    Update code formatting in solution.js

--- solution.js
+++ solution.js
@@ -1,29 +1,25 @@
-/**
- * @param {string} s
- * @return {number}
- */
 function longestValidParentheses(s) {
-    let stack = [-1];
-    let maxLength = 0;
+  const stack = [-1];
+  let maxLength = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(') {
-            stack.push(i);
-        } else {
-            stack.pop();
-            if (stack.length === 0) {
-                stack.push(i);
-            } else {
-                maxLength = Math.max(maxLength, i - stack[stack.length - 1]);
-            }
-        }
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(i);
+    } else {
+      stack.pop();
+
+      if (stack.length === 0) {
+        stack.push(i);
+      } else {
+        maxLength = Math.max(maxLength, i - stack[stack.length - 1]);
+      }
     }
+  }
 
-    return maxLength;
+  return maxLength;
 }
 
-// Test Cases
-console.log(longestValidParentheses("(()"));      // 2
-console.log(longestValidParentheses(")()())"));   // 4
-console.log(longestValidParentheses(""));         // 0
-console.log(longestValidParentheses("()(())"));   // 6
+console.log(longestValidParentheses("(()"));
+console.log(longestValidParentheses(")()())"));
+console.log(longestValidParentheses(""));
+console.log(longestValidParentheses("()(())"));
